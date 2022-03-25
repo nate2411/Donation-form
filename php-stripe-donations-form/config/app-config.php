7dd7f027f7ee01bb7ce553aff7caf34e3ef53292
nate2411 <82214146+nate2411@users.noreply.github.com>
2022-03-25 12:56:19 +0200

    Add files via upload

---
+++ php-stripe-donations-form/config/app-config.php
@@ -0,0 +1,6 @@
+<?php
+
+Config settings
+define('STRIPE_REQUIRE_HTTPS', true);
+define('STRIPE_API_KEY', 'sk_test_51Kh8f8CrxE19p0jxqmyflCiNQrBYMoqTtkHyvTXZYJstgq7H9Il8MaQXZPcBRazt4VY9Uvub6tYo388tZwsEiNzQ00YAF9CzdW');
+define('STRIPE_PUBLISHABLE_KEY', 'pk_test_51Kh8f8CrxE19p0jxe0TazunvyNdkQ8VpqboTwERBnwaIZyEgvmcLANmFqQEu9wRYYDqeT6FwTMCkcIbVoEdPesj300pssaZpPW');
